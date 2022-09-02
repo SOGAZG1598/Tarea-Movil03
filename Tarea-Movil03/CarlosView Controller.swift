348c88c273e4974262f3e121c948b3dcca73acca
charlyv01 <88901402+Charlyval01@users.noreply.github.com>
2022-09-02 09:08:41 -0500

    Charly

---
+++ Tarea-Movil03/CarlosView Controller.swift
@@ -0,0 +1,12 @@
+//
+//  CarlosV View Controller.swift
+//  Tarea-Movil03
+//
+//  Created by Carlos Valmaña Morales on 02/09/22.
+//
+
+import UIKit
+
+class CarlosV_View_Controller: NSObject {
+
+}
---
+++ Tarea-Movil03/CarlosViewController.swift
@@ -0,0 +1,32 @@
+//
+//  CarlosViewController.swift
+//  Tarea-Movil03
+//
+//  Created by Carlos Valmaña Morales on 02/09/22.
+//
+
+import UIKit
+
+class CarlosViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func CarlosBack(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
