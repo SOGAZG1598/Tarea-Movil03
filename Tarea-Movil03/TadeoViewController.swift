b318a027d50d46f4fa6ccc2b45c59c1ca9ce3999
Carlos Tadeo <carlostadeo_0125@hotmail.com>
2022-09-02 09:37:35 -0500

    Carlos Tadeo Perez Capistran

---
+++ Tarea-Movil03/TadeoViewController.swift
@@ -0,0 +1,31 @@
+//
+//  TadeoViewController.swift
+//  Tarea-Movil03
+//
+//  Created by Alumno on 02/09/22.
+//
+
+import UIKit
+
+class TadeoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func carlosBack(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
